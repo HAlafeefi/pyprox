644883b8538d31fb84a637c6274644fd86ad3219
Hamad <hamad@alafeefi.com>
2024-05-05 21:36:17 +0400

    init

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ def rewrite_links(base_url, html_content):
     # Rewrite relative links to absolute links
     soup = BeautifulSoup(html_content, 'html.parser')
     print(123454)
-    for tag in soup.find_all(['a', 'link', 'script', 'img']):
-        if 'href' in tag.attrs:
-            tag['href'] = get_absolute_url(base_url, tag['href'])
-        if 'src' in tag.attrs:
-            tag['src'] = get_absolute_url(base_url, tag['src'])
-            # print(tag['src'])
+    # for tag in soup.find_all(['a', 'link', 'script', 'img']):
+    #     if 'href' in tag.attrs:
+    #         tag['href'] = get_absolute_url(base_url, tag['href'])
+    #     if 'src' in tag.attrs:
+    #         tag['src'] = get_absolute_url(base_url, tag['src'])
+    #         # print(tag['src'])
     return str(soup)
 
 
