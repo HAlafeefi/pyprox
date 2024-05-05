c75c44719e8083d95236341562e07c06e6cde3c9
Hamad <hamad@alafeefi.com>
2024-05-05 20:25:08 +0400

    v2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 import requests
 
 app = Flask(__name__)
@@ -8,20 +8,24 @@ def add_schema(url):
         url = 'https://' + url
     return url
 
-# Define a route for the GET request
-@app.route('/')
-def index():
-    return '<h1>Deployed!</h1><style>body { display: flex; align-items: center; justify-content: center; height: 100vh; }</style>'
-@app.route('/url/<path:url>', methods=['GET'])
+
+
+@app.route('/', methods=['GET'])
 def get_url(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0',
     }
 
-    try:
-        print(add_schema(url))
-        request = requests.get(add_schema(url), headers=headers)
-        request.raise_for_status()
-        return request.content.decode('utf-8')
-    except requests.exceptions.RequestException as s:
-        abort(400, description=s)
+    url_param = request.args.get('url')
+
+    if url_param is None:
+        # Render a template or return a custom message
+        return '<h1>Deployed!</h1><style>body { display: flex; align-items: center; justify-content: center; height: 100vh; }</style>'
+    elif url_param:
+        try:
+            print(add_schema(url))
+            request = requests.get(add_schema(url), headers=headers)
+            request.raise_for_status()
+            return request.content.decode('utf-8')
+        except requests.exceptions.RequestException as s:
+            abort(400, description=s)
