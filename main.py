1e99e9052760370f346fe434dd95685e7bfb79be
Hamad <hamad@alafeefi.com>
2024-05-05 20:42:23 +0400

    init

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def add_schema(url):
         url = 'https://' + url
     return url
 
-
+def sanitize_url(url):
+    return url.replace('-', '.')
 
 @app.route('/search', methods=['GET'])
 def get_url():
@@ -17,13 +18,13 @@ def get_url():
     }
 
     url_param = request.args.get('q')
-
+    print(url_param)
     if url_param is None:
         # Render a template or return a custom message
         return '<h1>Deployed!</h1><style>body { display: flex; align-items: center; justify-content: center; height: 100vh; }</style>'
     elif url_param:
         try:
-            request1 = requests.get(add_schema(url_param), headers=headers)
+            request1 = requests.get(sanitize_url(add_schema(url_param)), headers=headers)
             request1.raise_for_status()
             return request1.content.decode('utf-8')
         except requests.exceptions.RequestException as s:
